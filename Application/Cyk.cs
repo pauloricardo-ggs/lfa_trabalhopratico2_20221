7120814981d4ad8ba57d021cf1711015b2940454
Paulo Ricardo <pauloricardo.ggs@gmail.com>
2022-06-05 14:30:22 -0300

    Implementação CYK

---
+++ Application/Cyk.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Application
+{
+    public class Cyk
+    {
+        public Cyk(Gramatica gramatica)
+        {
+            Gramatica = gramatica;
+        }
+
+        public Gramatica Gramatica { get; private set; }
+        public string PalavraInteira { get; private set; }
+        public char[] Palavra { get; private set; }
+        public int N { get; private set; }
+        public List<Regra>[,] Tabela { get; private set; }
+
+        public bool TestarPalavra(string palavra)
+        {
+            FormatarPalavra(palavra);
+
+            for (int j = 1; j <= N; j++)
+            {
+                var regras = Gramatica.ObterRegras(Palavra[j-1].ToString());
+                if (regras.Any())
+                {
+                    Tabela[1, j] = regras;
+                }
+            }
+
+            for (int i = 2; i <= N; i++)
+            {
+                for (int j = 1; j <= N-i+1; j++)
+                {
+                    for (int k = 1; k <= i-1; k++)
+                    {
+                        // Para cada regra que possui uma producao do estilo X => YZ
+                        foreach (var regra in Gramatica.Regras)
+                        {
+                            foreach(var producao in regra.Producoes.Where(producao => producao.Elementos.Count > 1))
+                            {
+                                var Y = Gramatica.ObterRegra(producao.Elementos[0].ToString());
+                                var Z = Gramatica.ObterRegra(producao.Elementos[1].ToString());
+
+                                if (Tabela[k, j].Contains(Y) && Tabela[i-k, j+k].Contains(Z))
+                                {
+                                    Tabela[i, j].Add(regra);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Tabela[N, 1].Contains(Gramatica.ObterRegra("S"))) return true;
+            return false;
+        }
+        public void FormatarPalavra(string palavra)
+        {
+            PalavraInteira = palavra;
+            Palavra = palavra.ToCharArray();
+
+            N = Palavra.Length;
+            Tabela = new List<Regra>[N+1, N+1];
+            for(int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N-i+1; j++)
+                {
+                    Tabela[i, j] = new List<Regra>();
+                }
+            }
+        }
+    }
+}
--- Application/Program.cs
+++ Application/Program.cs
@@ -7,13 +7,64 @@ namespace Application
 {
     internal class Program
     {
+        private static Gramatica Gramatica { get; set; }
+        private static Cyk Cyk { get; set; }
+
         static void Main(string[] args)
         {
-            var gramatica = CriarGramatica();
-            Console.WriteLine(gramatica.Imprimir());
-            Console.ReadLine();
+            Gramatica = CriarGramatica();
+            Cyk = new Cyk(Gramatica);
+
+            Loop();
+
+            Console.WriteLine("hello world");
         }
 
+        public static void Loop()
+        {
+            while (true)
+            {
+                Console.Clear();
+                try
+                {
+                    var palavra = SolicitarPalavra();
+
+                    if (palavra == "=") break;
+                    TestarPalavra(palavra);
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"\nUm erro inesperado do tipo '{exception.Message}' aconteceu.");
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Pressione qualquer tecla testar outra palavra.");
+
+                Console.ReadKey();
+            }
+        }
+        public static string SolicitarPalavra()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Gramatica.Imprimir() + "\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Insira a palavra de teste ('=' para sair): ");
+            return Console.ReadLine();
+        }
+        public static void TestarPalavra(string palavra)
+        {
+            if (Cyk.TestarPalavra(palavra))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nA palavra {palavra} é aceita pela gramática.");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nA palavra {palavra} não é aceita pela gramática.");
+        }
         public static Gramatica CriarGramatica()
         {
             var gramatica = new Gramatica();
@@ -25,25 +76,24 @@ namespace Application
 
                 var producoes = new List<Producao>();
 
-                foreach (var producaoInteira in SepararProducoes(regras))
+                foreach (var producao in SepararProducoes(regras))
                 {
-                    producoes.Add(new Producao(producaoInteira.ToCharArray().ToList()));
+                    producoes.Add(new Producao(producao));
                 }
 
-                var indexRegra = gramatica.ContemNomeRegra(nome);
-                if (indexRegra == -1)
+                var regra = gramatica.ObterRegra(nome);
+                if (regra == null)
                 {
                     gramatica.AdicionarRegra(nome, producoes);
                 }
                 else
                 {
-                    gramatica.AdicionarProducaoARegra(indexRegra, producoes);
+                    regra.AdicionarProducoes(producoes);
                 }
             }
 
             return gramatica;
         }
-
         private static List<string> SepararProducoes(List<string> regras)
         {
             return (regras[1].Split("|").ToList());
